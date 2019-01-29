4e0f9802c07971917b730d24e5f21e755bdaf6be
Philippe Lumpkin <phiglump@ut.utm.edu>
2019-01-29 15:27:39 -0600

    Main Pain

--- MorePain(t)/MainWindow.xaml.cs
+++ MorePain(t)/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +24,52 @@ namespace MorePain_t_
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.Strokes.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes inkDA = new DrawingAttributes();
+
+            inkDA.Color = Colors.Black;
+            Canvas.DefaultDrawingAttributes = inkDA;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes inkColor = new DrawingAttributes();
+
+            inkColor.Color = Colors.Red;
+            Canvas.DefaultDrawingAttributes = inkColor;
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+            DrawingAttributes inkColor = new DrawingAttributes();
+
+            inkColor.Color = Colors.Green;
+            Canvas.DefaultDrawingAttributes = inkColor;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingAttributes inkColor = new DrawingAttributes();
+
+            inkColor.Color = Colors.Blue;
+            Canvas.DefaultDrawingAttributes = inkColor;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            double value = slider.Value;
+            Canvas.DefaultDrawingAttributes.Width = value;
+            Canvas.DefaultDrawingAttributes.Height = value;
         }
     }
 }
